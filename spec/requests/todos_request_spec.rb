81af637fb444cba09691b3a1e453184704e85240
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-08-31 23:34:29 +0300

    add test for 'GET /todos' api endpoint

--- spec/requests/todos_request_spec.rb
+++ spec/requests/todos_request_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Todos", type: :request do
 
+  #initialize the test data with the help of Bactory-bot
+  let!(:todos) { create_list(:todo, 10) }
+  let(:todo_id) { todos.first.id }
+
+
+  # Test suite for GET /todos
+  describe('GET /todos') do
+    #make an HTTP request before each example
+    before { get '/todos' }
+
+    it('returns todos') do
+      # Note 'json' is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eql(10)
+    end
+
+    it('returns status code 200') do
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
