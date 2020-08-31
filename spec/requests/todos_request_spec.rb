f066cb61bd67d64b896559c60fc3071f1e55ba9a
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-08-31 23:44:09 +0300

    Test suite for GET /todos/:id

--- spec/requests/todos_request_spec.rb
+++ spec/requests/todos_request_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Todos", type: :request do
     it('returns todos') do
       # Note 'json' is a custom helper to parse JSON responses
       expect(json).not_to be_empty
-      expect(json.size).to eql(10)
+      expect(json.size).to eq(10)
     end
 
     it('returns status code 200') do
@@ -23,4 +23,32 @@ RSpec.describe "Todos", type: :request do
     end
   end
 
+  # Test suite for GET /todos/:id
+  describe('GET /todos/:id') do
+    before { get "/todos/#{todo_id}" }
+
+    context 'when the record exist' do
+      it 'returns the todo item' do
+        expect(json).not_to be_empty
+        expect(json[:id]).to eq(todo_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:todo_id) { 100 } # initialize with an id that does not exist
+
+      it 'should return status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'should  return a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+
+      end
+    end
+  end
 end
