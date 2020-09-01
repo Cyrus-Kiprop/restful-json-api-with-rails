281170b393b076cb3b55fbad1f5ffbf811e6502d
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-01 13:03:35 +0300

    add bcrypt dependency and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # encrypts the user password
+  has_secure_password
+
+  # Model Association
+  has_many :todos, foreign_key: :created_by
+  # Validation
+  validates_presence_of :name, :email, :password_digest
 end
